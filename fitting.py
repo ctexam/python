b0cd2c50025c27126f600c0ecbf8f6b7ebe40f0c
ctexam <mail@nived.space>
2025-06-26 13:33:24 +0530

    root

--- fitting.py
+++ fitting.py
@@ -1,48 +1,26 @@
 from pylab import*
-def determinant(a):
-    n=len(a)
-    if n==1:
-        return(a[0][0])
-    if n==2:
-        return (a[0][0]*a[1][1])-(a[0][1]*a[1][0])
-    det=0
-    for i in range(n):
-        b=delete(a,0,0)
-        b=delete(b,i,1)
-        det+=a[0][i]*(-1)**i*determinant(b)
-    return(det)
-
-def inv(f):
-    y=determinant(f)
-    k=len(f)
-    t=zeros((k,k),float)
-    for i in range(k):
-        for j in range(k):
-            b=delete(f,j,0)
-            b=delete(b,i,1)
-            t[i][j]=(((-1)**(i+j))*determinant(b))
-    s=t/y
-    return(s)
 x_data=[1,2,3,4,6,8]
-y_data=[2,3,4,6,5,10]
+y_data=[2,3,4,6,5,6]
 
 n=int(input("enter the degree of polynomial="))
-
-print(x_data) 
-print(y_data)
+#print(x_data) 
+#print(y_data)
 sum_matrix=zeros((n+1,n+1),int)
-sum_yx=zeros((n+1,1),int)
+sum_yx=zeros((n+1,1))
+print(sum_yx)
 for i in range(n+1):
     for j in range(n+1):
         sum_matrix[i][j]=sum(x**(i+j) for x in x_data)
 print(sum_matrix)
 
-
 for j in range(n+1):
     sum_yx[j]=sum(y * x**j for x, y in zip(x_data, y_data))
-invmat=(inv(sum_matrix))
+invmat=(linalg.inv(sum_matrix))
+print((sum_matrix))
+print(sum_yx)
+print(invmat)
 ans=dot(invmat,sum_yx)
-print("ans",ans)
+print(ans)
 po=len(ans)
 x =linspace(1, 8, 256)
 
@@ -51,10 +29,6 @@ polyn=poly1d(coefficents[::-1])
 print(polyn)
 y=polyn(x)
 
-plot(x,y, label="fitted polynomial")
-plot(x_data,y_data,':dg',label="orginal data points")
-xlabel("x")
-ylabel("f(x)")
-legend()
-show()
+plot(x,y)
+plot(x_data,y_data,':dg')
 show()
---
+++ regulafalsi.py
@@ -0,0 +1,18 @@
+from math import*
+def f(x):
+	return (x*exp(x)-2)
+a=float(input('Enter a value for which f(x) is negative(a):'))
+b=float(input('Enter a value for which f(x) is positive(b):'))
+while True:
+	x=(a*f(b)-b*f(a))/(f(b)-f(a))
+	if f(x)==0:
+		print('The exact root is:',x)
+		break
+	elif abs(f(x))<0.0001:
+		print('The root is:',x)
+		break
+	elif f(a)* f(b) < 0:
+            b = x
+	else:
+		a = x
+print("value of f(x) is:",f(x))
